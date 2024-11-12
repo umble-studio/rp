7f3158f26000af21785c43b62485eea27a75c400
bub.bl <bub.bl@outlook.com>
2024-11-12 15:53:34 +0100

    Generate equality members for CharacterId

--- Code/Core/CharacterId.cs
+++ Code/Core/CharacterId.cs
@@ -3,7 +3,7 @@ using System.Text.Json.Serialization;
 
 namespace Rp.Core;
 
-public readonly struct CharacterId
+public readonly struct CharacterId : IEquatable<CharacterId>
 {
 	[JsonPropertyName( "steamId" )] public ulong SteamId { get; }
 	[JsonPropertyName( "id" )] public ushort Id { get; }
@@ -18,14 +18,29 @@ public readonly struct CharacterId
 	public static implicit operator CharacterId( string id )
 	{
 		var parts = id.Split( '_' );
-		
+
 		if ( parts.Length is not 2 )
 			throw new Exception( "CharacterId format is invalid." );
-		
+
 		var steamId = ulong.Parse( parts[0] );
 		var characterId = ushort.Parse( parts[1] );
 
 		return new CharacterId( steamId, characterId );
+	}
+
+	public bool Equals( CharacterId other )
+	{
+		return SteamId == other.SteamId && Id == other.Id;
+	}
+
+	public override bool Equals( object? obj )
+	{
+		return obj is CharacterId other && Equals( other );
+	}
+
+	public override int GetHashCode()
+	{
+		return HashCode.Combine( SteamId, Id );
 	}
 
 	public override string ToString() => $"{SteamId}_{Id}";
