9817ad2690472b79c49eeea4ef96a4b3c67fe796
bub.bl <bub.bl@outlook.com>
2024-11-12 15:32:45 +0100

    Reference the new character in the player data

--- Code/Core/Managers/CharacterManager.Server.Commands.cs
+++ Code/Core/Managers/CharacterManager.Server.Commands.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using RoverDB;
 
 namespace Rp.Core.Managers;
 
 public partial class CharacterManager
 {
 	[ConCmd( "character_create" )]
-	private static void ServerCreateCharacterCmd( string firstname, string lastname )
+	private static void ServerCreateCharacterCmd( ulong steamId, string firstname, string lastname )
 	{
+		if ( !Networking.IsHost ) return;
+
+		var player = RoverDatabase.Instance.SelectOne<PlayerData>( x => x.Owner == steamId );
+		if ( player is null ) return;
+
 		var character = new CharacterData { CharacterId = Guid.NewGuid(), Firstname = firstname, Lastname = lastname, };
+
+		player.Characters.Add( character.CharacterId );
+		player.CurrentCharacter = character.CharacterId;
 
 		Log.Info( "Creating character: " + character.CharacterId );
 		Instance.CreateCharacterRpcRequest( character );
