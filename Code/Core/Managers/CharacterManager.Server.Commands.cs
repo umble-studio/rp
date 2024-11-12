d2abb3eb9ed1058bc17b73123f80adcc9ba3247f
bub.bl <bub.bl@outlook.com>
2024-11-12 15:40:27 +0100

    Use the local steam id instead of define them each time, we only use this command for debug purpose

--- Code/Core/Managers/CharacterManager.Server.Commands.cs
+++ Code/Core/Managers/CharacterManager.Server.Commands.cs
@@ -6,11 +6,11 @@ namespace Rp.Core.Managers;
 public partial class CharacterManager
 {
 	[ConCmd( "character_create" )]
-	private static void ServerCreateCharacterCmd( ulong steamId, string firstname, string lastname )
+	private static void ServerCreateCharacterCmd( string firstname, string lastname )
 	{
 		if ( !Networking.IsHost ) return;
 
-		var player = RoverDatabase.Instance.SelectOne<PlayerData>( x => x.Owner == steamId );
+		var player = RoverDatabase.Instance.SelectOne<PlayerData>( x => x.Owner == SteamId.Local );
 		if ( player is null ) return;
 
 		var character = new CharacterData { CharacterId = Guid.NewGuid(), Firstname = firstname, Lastname = lastname, };
