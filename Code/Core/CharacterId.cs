3354017eb210c2203318a0d27d3a8c8a43955960
bub.bl <bub.bl@outlook.com>
2024-11-12 15:52:28 +0100

    Create a CharacterId struct that store steam id + a short id

---
+++ Code/Core/CharacterId.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Rp.Core;
+
+public readonly struct CharacterId
+{
+	[JsonPropertyName( "steamId" )] public ulong SteamId { get; }
+	[JsonPropertyName( "id" )] public ushort Id { get; }
+
+	[JsonConstructor]
+	public CharacterId( ulong steamId, ushort id )
+	{
+		SteamId = steamId;
+		Id = id;
+	}
+
+	public static implicit operator CharacterId( string id )
+	{
+		var parts = id.Split( '_' );
+		
+		if ( parts.Length is not 2 )
+			throw new Exception( "CharacterId format is invalid." );
+		
+		var steamId = ulong.Parse( parts[0] );
+		var characterId = ushort.Parse( parts[1] );
+
+		return new CharacterId( steamId, characterId );
+	}
+
+	public override string ToString() => $"{SteamId}_{Id}";
+}
--- Code/Core/Converters/CharacterIdJsonConverter.cs
+++ Code/Core/Converters/CharacterIdJsonConverter.cs
@@ -1,25 +1,24 @@
-﻿namespace Rp.Core.Converters;
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
-// public sealed class CharacterIdJsonConverter : JsonConverter<CharacterId>
-// {
-// 	public override CharacterId Read( ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options )
-// 	{
-// 		var stringValue = reader.GetString();
-//
-// 		if ( stringValue is null )
-// 			throw new JsonException( "Unable to deserialize CharacterId because the input string was null." );
-//
-// 		var parts = stringValue.Split( '_' );
-//
-// 		if ( parts.Length is not 2 || !ushort.TryParse( parts[1], out var id ) )
-// 			throw new JsonException( "CharacterId format is invalid." );
-//
-// 		var steamId = SteamIdUtility.Parse( parts[0] );
-// 		return new CharacterId( steamId, id );
-// 	}
-//
-// 	public override void Write( Utf8JsonWriter writer, CharacterId value, JsonSerializerOptions options )
-// 	{
-// 		writer.WriteStringValue( value.ToString() );
-// 	}
-// }
+namespace Rp.Core.Converters;
+
+public sealed class CharacterIdJsonConverter : JsonConverter<CharacterId>
+{
+	public override CharacterId Read( ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options )
+	{
+		var stringValue = reader.GetString();
+
+		if ( stringValue is null )
+			throw new JsonException(
+				$"Unable to deserialize {nameof(CharacterId)} because the input string was null." );
+
+		return stringValue;
+	}
+
+	public override void Write( Utf8JsonWriter writer, CharacterId value, JsonSerializerOptions options )
+	{
+		writer.WriteStringValue( value.ToString() );
+	}
+}
