1b40fa130080ff6666d148ba0cb9f06c051147d7
bub.bl <bub.bl@outlook.com>
2024-11-12 15:39:56 +0100

    Add docs to network initializer

--- Code/Core/Systems/INetworkInitializer.cs
+++ Code/Core/Systems/INetworkInitializer.cs
@@ -2,13 +2,25 @@
 
 public interface INetworkInitializer
 {
+	/// <summary>
+	/// Called automatically when the client is loaded
+	/// </summary>
 	public interface IClient
 	{
+		/// <summary>
+		/// Called automatically when the client is loaded
+		/// </summary>
 		void InitializeClient();
 	}
 
+	/// <summary>
+	/// Called automatically when a client is connected to the server
+	/// </summary>
 	public interface IServer
 	{
+		/// <summary>
+		/// Called automatically when a client is connected to the server
+		/// </summary>
 		void InitializeServer( Connection channel );
 	}
 }
