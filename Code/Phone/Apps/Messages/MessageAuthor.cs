e05e3ce457d31f4c737726a7d42d39f782668f7c
bub.bl <bub.bl@outlook.com>
2024-11-13 02:04:27 +0100

    Make MessageAuthor a readonly struct

--- Code/Phone/Apps/Messages/MessageAuthor.cs
+++ Code/Phone/Apps/Messages/MessageAuthor.cs
@@ -1,6 +1,33 @@
-﻿namespace Rp.Phone.Apps.Messages;
+﻿using System;
 
-public record struct MessageAuthor : IPhoneNumber
+namespace Rp.Phone.Apps.Messages;
+
+public readonly struct MessageAuthor : IPhoneNumber, IEquatable<MessageAuthor>
 {
 	public PhoneNumber PhoneNumber { get; init; }
+
+	public bool Equals(MessageAuthor other)
+	{
+		return PhoneNumber.Equals(other.PhoneNumber);
+	}
+
+	public override bool Equals(object? obj)
+	{
+		return obj is MessageAuthor other && Equals(other);
+	}
+
+	public override int GetHashCode()
+	{
+		return PhoneNumber.GetHashCode();
+	}
+
+	public static bool operator ==( MessageAuthor left, MessageAuthor right )
+	{
+		return left.Equals( right );
+	}
+
+	public static bool operator !=( MessageAuthor left, MessageAuthor right )
+	{
+		return !(left == right);
+	}
 }
