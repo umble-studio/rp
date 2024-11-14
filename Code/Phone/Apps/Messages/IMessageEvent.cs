7b3eeea8a127a0f414479cfe7109c9ab15424c74
bub.bl <bub.bl@outlook.com>
2024-11-14 17:51:17 +0100

    Add new conversation events

--- Code/Phone/Apps/Messages/IMessageEvent.cs
+++ Code/Phone/Apps/Messages/IMessageEvent.cs
@@ -1,7 +1,11 @@
-﻿namespace Rp.Phone.Apps.Messages;
+﻿using System;
+
+namespace Rp.Phone.Apps.Messages;
 
 public interface IMessageEvent : ISceneEvent<IMessageEvent>
 {
 	void OnMessageSent( MessageData messageData ) { }
 	void OnMessageReceived( MessageData messageData ) { }
+	void OnConversationCreated( ConversationData conversationData ) { }
+	void OnConversationRemoved( Guid conversationId ) { }
 }
