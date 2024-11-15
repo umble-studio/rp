c3a14d0d8f182d26ebd2617b99bebf19cff04e63
bub.bl <bub.bl@outlook.com>
2024-11-15 13:39:14 +0100

    Move response rpc into client rpc

--- Code/Phone/Apps/Messages/Services/ConversationService.Client.Rpc.cs
+++ Code/Phone/Apps/Messages/Services/ConversationService.Client.Rpc.cs
@@ -1,4 +1,7 @@
-﻿namespace Rp.Phone.Apps.Messages.Services;
+﻿using System;
+using Rp.UI.Extensions;
+
+namespace Rp.Phone.Apps.Messages.Services;
 
 public partial class ConversationService
 {
@@ -16,5 +19,27 @@ public partial class ConversationService
 	{
 		_conversations = conversations;
 		Log.Info( "LoadConversationsRpcResponse: " + conversations.Count );
+	}
+	
+	[Broadcast( NetPermission.HostOnly )]
+	private void SendMessageRpcResponse( Guid conversationId, MessageData message )
+	{
+		var conversation = Conversations.FirstOrDefault( x => x.Id == conversationId );
+
+		// If one of the participant didn't have opened there phone
+		// The conversation will be null because conversation are loaded when the phone is opened
+		if ( conversation is null )
+		{
+			// We load conversations to be sure that the conversation is loaded
+			// Maybe need to wait a bit before getting the conversation,
+			// Because the conversation might not be inserted in the database yet
+			LoadConversations();
+
+			conversation = Conversations.FirstOrDefault( x => x.Id == conversationId );
+			if ( conversation is null ) return;
+		}
+
+		conversation.Messages.Add( message );
+		Scene.RunEvent<IMessageEvent>( x => x.OnMessageReceived( message ), true );
 	}
 }
--- Code/Phone/Apps/Messages/Services/ConversationService.Server.Rpc.cs
+++ Code/Phone/Apps/Messages/Services/ConversationService.Server.Rpc.cs
@@ -97,26 +97,4 @@ public partial class ConversationService
 			SendMessageRpcResponse( conversationId, message );
 		}
 	}
-
-	[Broadcast( NetPermission.HostOnly )]
-	private void SendMessageRpcResponse( Guid conversationId, MessageData message )
-	{
-		var conversation = Conversations.FirstOrDefault( x => x.Id == conversationId );
-
-		// If one of the participant didn't have opened there phone
-		// The conversation will be null because conversation are loaded when the phone is opened
-		if ( conversation is null )
-		{
-			// We load conversations to be sure that the conversation is loaded
-			// Maybe need to wait a bit before getting the conversation,
-			// Because the conversation might not be inserted in the database yet
-			LoadConversations();
-
-			conversation = Conversations.FirstOrDefault( x => x.Id == conversationId );
-			if ( conversation is null ) return;
-		}
-
-		conversation.Messages.Add( message );
-		Scene.RunEvent<IMessageEvent>( x => x.OnMessageReceived( message ), true );
-	}
 }
