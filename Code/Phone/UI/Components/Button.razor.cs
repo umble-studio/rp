a8d1276665f514748a3eb33cd5cc9a5e950f6775
bub.bl <bub.bl@outlook.com>
2024-11-15 16:47:04 +0100

    A theme for button (dark not supported yet)

--- Code/Phone/UI/Components/Button.razor.cs
+++ Code/Phone/UI/Components/Button.razor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Rp.UI;
 using Sandbox.UI;
 
 namespace Rp.Phone.UI.Components;
@@ -7,12 +8,20 @@ public sealed partial class Button : Panel
 {
 	public string Text { get; set; } = null!;
 	public string Icon { get; set; } = null!;
+	public string IconColor { get; set; } = "dodgerblue";
+	public PhoneTheme Theme { get; set; } = PhoneTheme.Light;
 	public Action? Action { get; set; }
 
+	private string Root => new CssBuilder()
+		.AddClass("light", Theme is PhoneTheme.Light)
+		.AddClass("dark", Theme is PhoneTheme.Dark)
+		.AddClass("transparent", Theme is PhoneTheme.Transparent)
+		.Build();
+	
 	protected override void OnClick( MousePanelEvent e )
 	{
 		Action?.Invoke();
 	}
 
-	protected override int BuildHash() => HashCode.Combine( Text, Icon );
+	protected override int BuildHash() => HashCode.Combine( Text, Icon, IconColor, Theme );
 }
