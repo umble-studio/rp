b272c78048740e2572e93bab95ca27a81cd976e2
bub.bl <bub.bl@outlook.com>
2024-11-17 14:46:34 +0100

    Phone message should use cascade too, needed to fix a lot of NRE exceptions

--- Code/Phone/Apps/Messages/Components/Message.razor.cs
+++ Code/Phone/Apps/Messages/Components/Message.razor.cs
@@ -4,17 +4,25 @@ using Sandbox.UI;
 
 namespace Rp.Phone.Apps.Messages.Components;
 
-public sealed partial class Message : Panel
+public sealed partial class Message : PhoneWidget
 {
 	public ConversationParticipant Participant { get; set; } = null!;
 	public MessageData Data { get; set; } = null!;
 	public bool ShowDate { get; set; }
 
+	private PhoneNumber PhoneNumber { get; set; }
+	
 	private string Root => new CssBuilder()
-		.AddClass( "me", Data.IsMe() )
-		.AddClass( "other", !Data.IsMe() )
-		.AddClass("space", ShowDate)
+		.AddClass( "me", Data.IsMe( PhoneNumber ) )
+		.AddClass( "other", !Data.IsMe( PhoneNumber ) )
+		.AddClass( "space", ShowDate )
 		.Build();
+	
+	protected override void OnAfterRender( bool firstRender )
+	{
+		Log.Info("OnAfterRender");
+		PhoneNumber = Phone.SimCard!.PhoneNumber;
+	}
 
-	protected override int BuildHash() => HashCode.Combine( Data, ShowDate, DateTime.Now.Second );
+	protected override int ShouldRender() => HashCode.Combine( Data, ShowDate, DateTime.Now.Second );
 }
