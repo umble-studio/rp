ba31cad9cece42f62166c23ef3197a414e11e0a1
bub.bl <bub.bl@outlook.com>
2024-11-17 14:41:53 +0100

    Fix NRE for AppIcon

--- Code/Phone/UI/Components/AppIcon.razor.cs
+++ Code/Phone/UI/Components/AppIcon.razor.cs
@@ -1,20 +1,32 @@
 ﻿using System;
+using Rp.UI;
 using Sandbox.UI;
 
 namespace Rp.Phone.UI.Components;
 
-public sealed partial class AppIcon : Panel
+public sealed partial class AppIcon : PhoneWidget
 {
 	public IPhoneApp App { get; set; } = null!;
 	public bool ShowTitle { get; set; } = true;
 
-	private int Notifications =>
-		(App as IAppNotifiable)?.GetPendingNotifications( App ).Count() ?? 0;
+	// private int Notifications =>
+	// 	(App as IAppNotifiable)?.GetPendingNotifications( App ).Count() ?? 0;
+
+	// private int Notifications =>
+	// 	App.Phone.Notification.GetPendingNotifications( App ).Count();
+
+	private int Notifications { get; set; }
+
+	protected override void OnAfterRender( bool firstRender )
+	{
+		Notifications = App.Phone.Notification.GetPendingNotifications( App ).Count();
+	}
 
 	protected override void OnClick( MousePanelEvent e )
 	{
-		Phone.Current.SwitchToApp( App );
+		App.Phone.SwitchToApp( App );
 	}
 
-	protected override int BuildHash() => HashCode.Combine( App, Notifications, ShowTitle );
+	protected override int ShouldRender() =>
+		HashCode.Combine( base.ShouldRender(), App, Notifications, ShowTitle );
 }
