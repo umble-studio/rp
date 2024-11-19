e9b93d5d9824bc0548fcb9f1b77be398278a36ea
bub.bl <bub.bl@outlook.com>
2024-11-19 17:47:12 +0100

    Make a copy of the favorite page into the contacts page

--- Code/Phone/Apps/FaceTime/Components/ContactsTab.razor.cs
+++ Code/Phone/Apps/FaceTime/Components/ContactsTab.razor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Rp.Phone.Apps.FaceTime.Services;
 using Rp.Phone.UI.Components;
 using Rp.UI;
 
@@ -7,6 +8,26 @@ namespace Rp.Phone.Apps.FaceTime.Components;
 public sealed partial class ContactsTab : PhoneNavigationPage
 {
 	private MessageBar _navigationBar = null!;
-	
+
 	public override string PageName => "Contacts";
+
+	private List<PhoneContact> Contacts { get; set; } = new();
+
+	protected override void OnAfterRender( bool firstRender )
+	{
+		Contacts = Phone.Contacts.All;
+	}
+
+	private void OnSelectContact( PhoneContact contact )
+	{
+		var callService = Phone.Local.GetService<CallService>();
+
+		var canCall = callService.StartOutgoingCall( contact.ContactNumber );
+		if ( !canCall ) return;
+
+		var tab = Host.Navigate<CallTab>();
+		tab.ShowPendingCallView( contact );
+	}
+
+	protected override int ShouldRender() => HashCode.Combine( base.ShouldRender(), Phone.Contacts.All );
 }
