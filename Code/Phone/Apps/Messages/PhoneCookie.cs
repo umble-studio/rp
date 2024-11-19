b2810dab84dea5d7ddec94218882fa3081a33225
bub.bl <bub.bl@outlook.com>
2024-11-19 18:18:40 +0100

    Add phone cookie class to store some data during session

---
+++ Code/Phone/Apps/Messages/PhoneCookie.cs
@@ -0,0 +1,58 @@
+namespace Rp.Phone.Apps.Messages;
+
+public static class PhoneCookie
+{
+	private static readonly Dictionary<string, object> Cookies = new();
+
+	/// <summary>
+	/// Sets the value of a cookie.
+	/// </summary>
+	/// <param name="key">The key of the cookie.</param>
+	/// <param name="value">The value of the cookie.</param>
+	public static void SetCookie( string key, object value ) => Cookies[key] = value;
+
+	/// <summary>
+	/// Removes a cookie.
+	/// </summary>
+	/// <param name="key">The key of the cookie.</param>
+	public static void RemoveCookie( string key ) => Cookies.Remove( key );
+
+	/// <summary>
+	/// Checks if a cookie exists.
+	/// </summary>
+	/// <param name="key">The key of the cookie.</param>
+	/// <returns>True if the cookie exists, false otherwise.</returns>
+	public static bool HasCookie( string key ) => Cookies.ContainsKey( key );
+
+	/// <summary>
+	/// Gets the value of a cookie.
+	/// </summary>
+	/// <typeparam name="T">The type of the value.</typeparam>
+	/// <param name="key">The key of the cookie.</param>
+	/// <returns>The value of the cookie, or null if the cookie does not exist.</returns>
+	public static T? GetCookie<T>( string key ) => (T)Cookies[key];
+
+	/// <summary>
+	/// Tries to get the value of a cookie.
+	/// </summary>
+	/// <typeparam name="T">The type of the value.</typeparam>
+	/// <param name="key">The key of the cookie.</param>
+	/// <param name="value">The value of the cookie, or null if the cookie does not exist.</param>
+	/// <returns>True if the cookie exists, false otherwise.</returns>
+	public static bool TryGetCookie<T>( string key, out T value )
+	{
+		if ( Cookies.TryGetValue( key, out var val ) )
+		{
+			value = (T)val;
+			return true;
+		}
+
+		value = default!;
+		return false;
+	}
+
+	/// <summary>
+	/// Clears all cookies.
+	/// </summary>
+	public static void ClearCookies() => Cookies.Clear();
+}
